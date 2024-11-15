4d755e36b074484b44ba120c9375bc4e9b6278b5
CodeWithInferno2618 <prathambiren2618@gmail.com>
2024-11-14 23:39:51 -0500

    fix: Remove merge conflict markers in google.js

--- src/lib/google.js
+++ src/lib/google.js
@@ -12,12 +12,11 @@ export const fetchUserEmails = async (userTokens) => {
     process.env.GOOGLE_CLIENT_SECRET,
 
     redirectUri
-<<<<<<< HEAD
+
     );
-=======
 
   );
->>>>>>> 8cc0078f704340e5c16459387b4d64898f5d3d24
+
 
   client.setCredentials({
     access_token: userTokens.access_token,
